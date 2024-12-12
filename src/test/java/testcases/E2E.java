0a85831a911119c4713352d068695a1fd65953f3
Salma Mohamed <sm6349589@gmail.com>
2024-12-12 13:11:21 +0200

    first commit

--- src/test/java/testcases/E2E.java
+++ src/test/java/testcases/E2E.java
@@ -1,4 +1,26 @@
 package testcases;
 
-public class E2E {
+import basics.Base;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class E2E extends Base {
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        return new Object[][]{
+                {"standard_user", "secret_sauce"},
+        };
+    }
+
+    @Test(dataProvider = "loginData")
+    public void loginWithValidCredentials(String userName, String password) {
+        loginPage.waitForPageLoad();
+        loginPage.setEmail(userName);
+        loginPage.setPassword(password);
+        loginPage.clickOnLogin();
+        homePage.waitForPageLoad();
+        Assert.assertTrue(homePage.isInventoryDisplayed(), "Inventory page is not displayed for user: " + userName);
+
+    }
 }
